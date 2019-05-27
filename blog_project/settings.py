66633ecf88e632ac8111cad88f71817f42f479fb
Collin <ococncol@gmail.com>
2019-05-27 16:06:21 -0400

    forms and user accounts

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # My Apps
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
